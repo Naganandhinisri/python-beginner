a793bd51fbbccb5b41aa465c0d5e3a407a2c3897
Techops <techops@Intern4-MacBook-Pro.local>
2019-01-28 18:07:28 +0530

    Initisl commit

---
+++ hello world.py
@@ -0,0 +1,8 @@
+a=int(input("enter the input  less than 60"))
+n=45
+while()
+if(a<n):
+    print('increase the number ')
+else:
+    print('decrease the number')
+continue
---
+++ task 2.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+while(count>0)
+if(n>50):
+    print('display a')
+    count++
+    else (count=quit)
+    print('quit')
