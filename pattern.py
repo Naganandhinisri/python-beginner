315054c1ccb66f5aacb6701c4dbb8f74d365324e
Techops <techops@Intern4-MacBook-Pro.local>
2019-01-29 10:33:37 +0530

    hai

---
+++ pattern.py
@@ -0,0 +1,8 @@
+rows=input("enter the number of rows")
+rows=int(rows)
+for i in range(0,rows):
+    for j in range(0,i+1):
+        print(" * " , end = ' ')
+    print("\r")
+
+print("height=number of rows",rows)
